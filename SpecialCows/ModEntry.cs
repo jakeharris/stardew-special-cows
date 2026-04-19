386510f8d5df29ad10fbec7f917ae15fb1bc2964
Jake Harris <milieu@javakat.io>
2026-04-18 21:48:27 -0700

    Flesh out crafting & cooking recipes, unlock mechanisms

--- SpecialCows/ModEntry.cs
+++ SpecialCows/ModEntry.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
@@ -6,6 +8,16 @@ namespace SpecialCows
 {
     public class ModEntry : Mod
     {
+        private const string DemetriusMailId = "StrawberryMilkMafia.SpecialCows_DemetriusCooking";
+
+        private static readonly HashSet<string> SpecialMilkIds = new()
+        {
+            "StrawberryMilkMafia.SpecialCows.CP_StrawberryMilk",
+            "StrawberryMilkMafia.SpecialCows.CP_LargeStrawberryMilk",
+            "StrawberryMilkMafia.SpecialCows.CP_ChocolateMilk",
+            "StrawberryMilkMafia.SpecialCows.CP_LargeChocolateMilk",
+        };
+
         private TransformationHandler _handler = null!;
 
         public override void Entry(IModHelper helper)
@@ -14,6 +26,11 @@ namespace SpecialCows
 
             _handler = new TransformationHandler(this.Monitor);
             helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.Player.InventoryChanged += this.OnInventoryChanged;
+
+            // TODO: implement DayStarted hook to queue StrawberryMilkMafia.SpecialCows_MarnieTea
+            // when all three conditions are met: 2 hearts Caroline, Caroline's sunroom event seen,
+            // and 2 hearts Marnie. Check Game1.player.mailReceived before queuing.
         }
 
         private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
@@ -25,6 +42,16 @@ namespace SpecialCows
             if (!e.Button.IsActionButton()) return;
 
             _handler.TryTransform(Game1.player, Game1.currentLocation);
+        }
+
+        private void OnInventoryChanged(object? sender, InventoryChangedEventArgs e)
+        {
+            if (!Context.IsWorldReady || !e.IsLocalPlayer) return;
+            if (Game1.player.mailReceived.Contains(DemetriusMailId)) return;
+
+            bool gotSpecialMilk = e.Added.Any(item => SpecialMilkIds.Contains(item.ItemId));
+            if (gotSpecialMilk)
+                Game1.player.mailForTomorrow.Add(DemetriusMailId);
         }
     }
 }
